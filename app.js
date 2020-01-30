a6f1ea8c25e1ec88bf0bd8c1dfa1add763f0a6b6
david <david_519cs@hotmail.com>
2020-01-30 16:03:43 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+new Vue({
+  el: '#app',
+
+  data(){
+    return{
+      name: 'Bitcoin',
+      img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+      changePercent: 10,
+
+      price: 8400,
+
+      pricesWithDays: [
+
+        {day: 'Lunes', value: 8400},
+        {day: 'Martes', value: 8600},
+        {day: 'Miércoles', value: 8200},
+        {day: 'Jueves', value: 8800},
+        {day: 'Viernes', value: 8100},
+        {day: 'Sábado', value: 8700},
+        {day: 'Domingo', value: 8900}
+
+      ],
+
+      showPrices: false
+    }
+  },
+
+//aqui defino funciones
+  methods:{
+    toggleShowPrices(){
+      this.showPrices =!this.showPrices
+    }
+  }
+})
